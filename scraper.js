f3dd3d3743f14a35986690c3938abd3b6ddc1b14
Clement Hoang <clement.hoang24@gmail.com>
2015-04-12 02:33:53 -0400

    Revamps scraper file with safer scraping and moves mongoURI into config file

--- scraper.js
+++ scraper.js
@@ -1,39 +1,119 @@
+'use strict';
+
 var request = require('request');
 var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+var transientEventCtrl = require('./controllers/transientEvent.controller.js');
+var async = require('async');
+var utilities = require('./utilities');
+var config = require('./config');
+
+mongoose.connect(config.mongoUri);
 
 module.exports = {
     repopulate: repopulate
 };
 
 function repopulate(req, res, next) {
-    var events = [];
 
-    request('http://integral.esac.esa.int/bexrbmonitor/webpage_oneplot.php', function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            $ = cheerio.load(html);
+    async.waterfall([
+        getHtml,
+        buildEvents,
+        //dropCollection,
+        saveEvents
+    ], finalCallback);
 
-            $('tbody tr').each(function(rowIndex, row) {
-                var eventData = {};
-                eventData['name'] = $(row).find('td div#name_cell').html().trim();
+    function getHtml(waterfallNext) {
+        request('http://integral.esac.esa.int/bexrbmonitor/webpage_oneplot.php', function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+                waterfallNext(error, html);
+            }
+        });
+    }
 
-                var columns = $(row).find('td div.formated_cell');
-                eventData['ra'] = $(columns[1]).html().trim();
-                eventData['dec'] = $(columns[2]).html().trim();
-                eventData['orbital_period'] = $(columns[3]).html().trim();
-                eventData['MAXI_flux_change_prob'] = $(row).find('td div.formated_cell span').html().trim();
-                eventData['MAXI_avg_flux_mCrab'] = $(columns[5]).html().trim();
-                eventData['MAXI_data_date'] = $(columns[6]).html().trim();
-                eventData['swift_avg_flux_change_prob'] = $(row).find('td div.formated_cell span').html().trim();
-                eventData['swift_avg_flux_mCrab'] = $(columns[8]).html().trim();
-                eventData['swift_data_date'] = $(columns[9]).html().trim();
-                eventData['fermi_flux_change_prob'] = $(columns[10]).html().trim();
-                eventData['fermi_average_pulsed_flux'] = $(columns[11]).html().trim(); // in keV cm^-2 s^-1
-                eventData['fermi_data_date'] = $(columns[12]).html().trim();
+    function buildEvents(html, waterfallNext) {
+        var events = [];
 
-                events.push(eventData);
-            });
+        var $ = cheerio.load(html);
 
-            res.send('# of events saved: ');
+        $('tbody tr').each(function (rowIndex, row) {
+            var eventData = {};
+            var temp;
+
+            eventData['name'] = $(row).find('td div#name_cell').html().trim();
+
+            var columns = $(row).find('td div.formated_cell');
+            eventData['ra'] = $(columns[1]).html().trim();
+            eventData['dec'] = $(columns[2]).html().trim();
+            eventData['orbital_period'] = $(columns[3]).html().trim();
+
+            temp = $(row).find('td div.formated_cell span').html().trim();
+            if (isDataValid(temp)) {
+                eventData['MAXI_flux_change_prob'] = temp.substring(0, temp.length-1)/100;
+            }
+
+            temp = $(columns[5]).html().trim();
+            if (isDataValid(temp)) {
+                eventData['MAXI_avg_flux_mCrab'] = temp;
+            }
+
+            temp = $(columns[6]).html().trim();
+            if (isDataValid(temp)) {
+                console.log(temp);
+                eventData['MAXI_data_date'] = utilities.formatDate(temp);
+            }
+
+            temp = $(row).find('td div.formated_cell span').html().trim(); //this is exact same as the one for MAXI...
+            if (isDataValid(temp)) {
+                eventData['swift_avg_flux_change_prob'] = temp.substring(0, temp.length-1)/100;
+            }
+
+            temp = $(columns[8]).html().trim();
+            if (isDataValid(temp)) {
+                eventData['swift_avg_flux_mCrab'] = temp;
+            }
+
+            temp = $(columns[9]).html().trim();
+            if (isDataValid(temp)) {
+                console.log(temp);
+                eventData['swift_data_date'] = utilities.formatDate(temp);
+            }
+
+            temp = $(columns[10]).html().trim();
+            if (isDataValid(temp)) {
+                eventData['fermi_flux_change_prob'] = temp.substring(0, temp.length-1)/100;
+            }
+
+            temp = $(columns[11]).html().trim(); // in keV cm^-2 s^-1
+            if (isDataValid(temp)) {
+                eventData['fermi_average_pulsed_flux'] = temp;
+            }
+
+            temp = $(columns[12]).html().trim();
+            if (isDataValid(temp)) {
+                console.log(temp);
+                eventData['fermi_data_date'] = utilities.formatDate(temp);
+            }
+
+            events.push(eventData);
+        });
+
+        waterfallNext(null, events);
+
+        function isDataValid(text) {
+            return text && text !== 'NO DATA' && text !== '-';
         }
-    });
+    }
+
+    function saveEvents(events, waterfallNext) {
+        transientEventCtrl.saveEvents(events, waterfallNext);
+    }
+
+    function finalCallback(error) {
+        if (error) {
+            next(error);
+        }
+
+        res.status(200).send({message: 'Successfully repopulated events.'});
+    }
 }
